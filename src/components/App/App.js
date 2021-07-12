84a5a6d7339b81f8bb64722d34dffe4f4cc704fc
khizrim <kh.makhmudov@me.com>
2021-07-12 22:25:26 +0300

    feat: add App component

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__container" />
+    </div>
+  );
+}
+
+export default App;
