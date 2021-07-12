394f7fead61d5ae7238d520b97cfe237b641412f
khizrim <kh.makhmudov@me.com>
2021-07-12 22:45:21 +0300

    feat: add header to app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Header from '../Header/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <div className="app__container" />
+      <div className="app__container">
+        <Header isLoggedIn />
+      </div>
     </div>
   );
 }
