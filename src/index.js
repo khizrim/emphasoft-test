eeae0644255fb8ae081a9be754159a5a8a6ea367
khizrim <kh.makhmudov@me.com>
2021-07-12 22:45:06 +0300

    feat: install react router and app component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './components/App/App';
+
 import './index.css';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <p>Hello, World!</p>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 
   document.getElementById('root'),
