67e8f9b1e8267ce0ac6bde60ef0aa9685351de71
khizrim <kh.makhmudov@me.com>
2021-07-17 22:19:03 +0300

    chore: change browser router to hash router for gh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './components/App/App';
 
@@ -10,9 +10,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename="/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 
   document.getElementById('root'),
